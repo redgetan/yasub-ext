e60a0e02dae459340f4b7a08b0c5cb7e6cb7d039
Reginald Tan <redge.tan@gmail.com>
2015-11-10 00:11:46 -0500

    working ^.^

--- browser/chrome/background.js
+++ browser/chrome/background.js
@@ -4,37 +4,46 @@ var naverPlayerToUrlMap = {};
 var sourceDownloadUrlMap = {};
 var naverEditorPort = null;
 var PROGRESS_QUERY_INTERVAL = 2000;
+var queryProgressTimeoutList = [];
 
-function queryProgress(query_progress_url, sendResponse) {
+function queryProgress(query_progress_url) {
   $.ajax({
     url: query_progress_url,
     method: "GET",
     dataType: "json",
     success: function(data) {
       if (data.new_repo_url) {
-        sendResponse({ new_repo_url: data.new_repo_url });
+        naverEditorPort.postMessage({ new_repo_url: data.new_repo_url });
       } else {
-        sendResponse({ progress: data.progress });
-        setTimeout(function(){
-          queryProgress(query_progress_url, sendResponse);
-        }, PROGRESS_QUERY_INTERVAL);
+        naverEditorPort.postMessage({ progress: data.progress });
+
+        if (data.progress === "100") {
+          for (var i = 0; i < queryProgressTimeoutList.length; i++) { 
+             clearTimeout(queryProgressTimeoutList[i]);
+          }
+        } else {
+          var timeout = setTimeout(function(){
+            queryProgress(query_progress_url);
+          }, PROGRESS_QUERY_INTERVAL);
+          
+          queryProgressTimeoutList.push(timeout);
+        }
       }
     }
   });
 }
 
-function downloadSource(videoUrl, sourceDownloadUrl, sendResponse) {
+function downloadSource(videoUrl, sourceDownloadUrl) {
   $.ajax({
     url: base_url + "/videos/prepare",
     method: "POST",
     data: { source_url: videoUrl, source_download_url: sourceDownloadUrl },
     dataType: "json",
     success: function(data) {
-      debugger
       if (data.new_repo_url) {
-        sendResponse({ new_repo_url: data.new_repo_url });
+        naverEditorPort.postMessage({ new_repo_url: data.new_repo_url });
       } else if (data.query_progress_url) {
-        queryProgress(data.query_progress_url, sendResponse);
+        queryProgress(data.query_progress_url);
       }
     }
   });
@@ -61,12 +70,19 @@ chrome.webRequest.onResponseStarted.addListener(function(data){
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.type === "naver_editor") {
-      var sourceDownloadUrl = chrome.storage.local.get(msg.url, function(item){
-        sourceDownloadUrl = item[msg.url];
-        downloadSource(msg.url, sourceDownloadUrl, sendResponse);
-        sendResponse("fuck this shit");
-      });
     }
     return true;
 });
 
+
+chrome.runtime.onConnect.addListener(function(port) {
+  if (port.name === "naver_editor") {
+    naverEditorPort = port;
+    port.onMessage.addListener(function(msg) {
+      chrome.storage.local.get(msg.url, function(item){
+        var sourceDownloadUrl = item[msg.url];
+        downloadSource(msg.url, sourceDownloadUrl);
+      });
+    });
+  }
+});
--- browser/chrome/popup.js
+++ browser/chrome/popup.js
@@ -2,29 +2,38 @@ $(document).ready(function(){
   $("#subtitle_btn").on("click", function(event){
     event.preventDefault();
 
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-      var url = tabs[0].url;
+    if ($(this).hasClass("ready")) {
+      console.log("creating tab: " + $(this).attr("href"));
+      chrome.tabs.create({ url: $(this).attr("href") });
+    } else {
+      prepareEditor();
+    }
 
-      var message = { 
-        type: "naver_editor",
-        url: url  
-      };
+  });
 
-      chrome.runtime.sendMessage(chrome.runtime.id, message, {}, function(response){
-        console.log("popup cb: " + response);
-        if (response.new_repo_url) {
-          $("#subtitle_btn").text("Ready");  
-          $("#subtitle_btn").addClass("ready");  
-          $("#subtitle_btn").attr("href",response.new_repo_url);  
-        } else if (response.progress) {
-          $("#progress_indicator").text(response.progress + " %");
-        }
-      });
+});
+
+function prepareEditor() {
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    var url = tabs[0].url;
+
+    var message = { 
+      type: "naver_editor",
+      url: url  
+    };
+
+    var port = chrome.runtime.connect({name: "naver_editor"});
+    port.postMessage({url: url});
+    port.onMessage.addListener(function(msg) {
+      console.log("popup cb: " + JSON.stringify(msg));
+      if (msg.new_repo_url) {
+        $("#subtitle_btn").text("Ready");  
+        $("#subtitle_btn").addClass("ready");  
+        $("#subtitle_btn").attr("href",msg.new_repo_url);  
+      } else if (msg.progress) {
+        $("#progress_indicator").text(msg.progress + " %");
+      }
     });
 
   });
-
-  $("#subtitle_btn.ready").on("click", function(event){
-    chrome.tabs.create({ url: $(this).attr("href") });
-  });
-});
+}
